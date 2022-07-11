11d8163b28fdd78fb07a72284a04eba4dee1663a
ffjaervik <100479956+ffjaervik@users.noreply.github.com>
2022-07-11 10:07:14 +0100

    Create Read & Update functions

--- db.sql
+++ db.sql
@@ -6,11 +6,6 @@
 
 
 
-CREATE TABLE restaurants (
-    id BIGSERIAL NOT NULL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    location VARCHAR(50) NOT NULL,
-    price_range INT NOT NULL check(price_range >= 1 and price_range <= 5)
-);
+
 
 INSERT INTO restaurants (id, name, location, price_range) VALUES (2, 'mcdonalds', 'new york', 1);
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 require("dotenv").config();
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 
 
+app.use(express.json());
+
+
 //GET ALL RESTAURANTS
-app.get("/api/v1/getRestaurants", (req, res) => {
+app.get("/api/v1/restaurants", (req, res) => {
+    console.log("GET ALL RESTAURANTS");
     res.status(200).json ({
         status:"success",
         data: {
@@ -15,14 +20,39 @@ app.get("/api/v1/getRestaurants", (req, res) => {
 });
 
 // GET RESTAURANT BY ID
-app.get("/api/v1/restaurant/:id", (req, res) => {
+app.get("/api/v1/restaurants/:id", (req, res) => {
     console.log(req.params);
+    res.status(200).json ({
+        status:"success",
+        data: {
+            restaurant: "mcdonalds"
+        }
+    });
 });
 
 //CREATE RESTAURANT
-app.post("/api/v1/restaurant", (req, res) => {
-    console.log(req);
+app.post("/api/v1/restaurants", (req, res) => {
+    console.log(req.body);
+    res.status(201).json ({
+        status:"success",
+        data: {
+            restaurant: "mcdonalds"
+        }
+    });
 });
+
+//UPDATE RESTAURANT
+app.put("/api/v1/restaurants/:id", (req, res) => {
+    console.log(req.params.id);
+    console.log(req.body);
+    res.status(200).json ({
+        status:"success",
+        data: {
+            restaurant: "mcdonalds"
+        }
+    });
+});
+
 
 
 const port = process.env.PORT || 3001;
